988b53c0ff917d0a3c9da7f2e3ecbd7439cfda80
KHW01104 <khw01104@naver.com>
2025-10-06 01:17:56 +0900

    [FEAT] 결제 기능 구현 - exception

---
+++ src/main/java/backend/auth/exception/PaymentNotFoundException.java
@@ -0,0 +1,7 @@
+package backend.auth.exception;
+
+public class PaymentNotFoundException extends RuntimeException {
+    public PaymentNotFoundException(String message) {
+        super(message);
+    }
+}
